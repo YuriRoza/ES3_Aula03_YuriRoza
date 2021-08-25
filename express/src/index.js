99fdb7080e746b6435445cf49e03045291590de1
Yuri da Roza <yurinobredaroza@gmail.com>
2021-08-24 21:49:01 -0300

    Adiciona status code ao projeto

--- express/src/index.js
+++ express/src/index.js
@@ -10,15 +10,28 @@ let clients = [
     {id: 5, nome: 'Jessica Jones', telefone: '53981070809'}
 ]
 
-app.get('/clients', (request, response) => response.json(clients));
+function log (request, response, next) {
+    const {url, method} = request;
+    console.log(`${method} - ${url} at ${new Date()}`);
+    next();
+};
+
+app.use(log);
+
+app.get('/clients', (request, response) => response.status(200).json(clients));
 
 app.get('/clients/:id', (request, response) => {
-    const client = clients.filter(value => value.id == request.params.id);
-    return response.json(client);
+    const {id} = request.params;
+    const client = clients.find(value => value.id == id);
+    if (client == undefined) {
+        response.status(400).json({error: 'Requisição Inválida'});
+    } else {
+        response.status(200).json(client);
+    }
 });
 
 /*
-Possível fazer com
+Possível fazer com:
 app.get('/clients/:id", (request, response) =>
 response.json(clients.filter(value => value.id == request.params.id)));
 */
@@ -26,24 +39,31 @@ response.json(clients.filter(value => value.id == request.params.id)));
 app.post('/clients', (request, response) => {
     const client = request.body;
     clients.push(client);
-    response.json(client);
+    response.status(201).json(client);
 });
 
 app.put('/clients/:id', (request, response) => {
-    const id = request.params.id;
+    const {id} = request.params;
     const nome = request.body.nome;
-
-    let client = clients.filter(value => value.id == id);
-
-    client[0].nome = nome;
-
-    response.json(client[0]);
+    
+    let client = clients.find(value => value.id == id);
+    if (client == undefined) {
+        response.status(400).send();
+    } else {
+    client.nome = nome;
+    response.status(200).json(client);
+    }
 });
 
 app.delete('/clients/:id', (request, response) =>{
-    const id = request.params.id;
-    clients = clients.filter(value => value.id != id);
-    response.json(clients);
+    const {id} = request.params;
+    const index = clients.findIndex(value => value.id == id);
+    if (index == - 1){
+        response.status(400).send();
+    } else {
+        clients.splice(index, 1);
+        response.status(204).send();
+    }
 });
 
 app.listen(3000);
